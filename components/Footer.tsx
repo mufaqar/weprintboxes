ef73896275213cde3a21801f83a37f2f03ba4fc0
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2026-04-27 20:06:07 +0500

    Copy

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -13,41 +13,40 @@ const Footer = () => {
                     <p className='text-sm font-normal text-white my-6'>
                         Turn every unboxing into a memorable moment with our eye-catching custom printed boxes, specially made only for you!
                     </p>
-                    <ul className='py-[18px] flex flex-wrap gap-[30px] justify-center items-center border-y border-white/20'>
-                        <li>
-                            <Link href="/about-us" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
-                                About Us
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/blog" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
-                                Blog
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/privacy-policy/" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
-                                Privacy Policy
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/refund-policy/" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
-                                Refund Policy
-                            </Link>
-                        </li>
-                        <li>
-                            <Link href="/terms-conditions/" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
-                                Terms & Conditions
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className='py-6'>
-                        <Image src="/images/payment.png" alt='payment' width={426} height={65} className='object-contain flex max-w-[438px] mx-auto h-fit bg-white py-1 px-4 rounded-md' />
-                    </div>
-                    <p className='text-xs text-white'>
-                        WE PRINT BOXES © 2026 All rights reserved
-                    </p>
                 </div>
-
+                <ul className='py-[18px] flex flex-wrap gap-[30px] justify-center items-center border-y border-white/20'>
+                    <li>
+                        <Link href="/about-us" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
+                            About Us
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/blog" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
+                            Blog
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/privacy-policy/" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
+                            Privacy Policy
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/refund-policy/" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
+                            Refund Policy
+                        </Link>
+                    </li>
+                    <li>
+                        <Link href="/terms-conditions/" className='text-sm font-semibold text-white hover:text-[#55daa4] hover:underline'>
+                            Terms & Conditions
+                        </Link>
+                    </li>
+                </ul>
+                <div className='py-6'>
+                    <Image src="/images/payment.png" alt='payment' width={426} height={65} className='object-contain flex max-w-[438px] mx-auto h-fit bg-white py-1 px-4 rounded-md' />
+                </div>
+                <p className='text-xs text-white text-center py-6'>
+                    WE PRINT BOXES © 2026 All rights reserved
+                </p>
             </div>
         </footer>
     )
